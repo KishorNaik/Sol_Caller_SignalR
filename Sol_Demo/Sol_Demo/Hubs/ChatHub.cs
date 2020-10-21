24558cc9976325d319d91747e721caa6b6e561a0
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-21 10:48:28 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Hubs
+{
+    public interface IChatClient
+    {
+        Task SendMessageToCallerJsMethod(string message);
+    }
+
+    public class ChatHub : Hub<IChatClient>
+    {
+        public async Task SendMessage(string message)
+        {
+            await base.Clients.Caller.SendMessageToCallerJsMethod(message);
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/wwwroot/js/chat.js
@@ -0,0 +1,63 @@
+﻿"use strict";
+
+// to Make SignalR connection. Step 1
+
+var connection = new signalR
+    .HubConnectionBuilder()
+    .withUrl("/chathub")
+    .withAutomaticReconnect()
+    .build();
+
+// Disable Send Button Step 2
+$("#btnSendMessage").prop("disabled", true);
+
+// Template for List Step 4
+function templateList(message) {
+    //console.log("Template :", user);
+    //console.log("Template :", message);
+
+    var template = `<li>myself  : ${message}</li>`;
+
+    return template;
+}
+
+// SingnalR On connection, received message from Server. Step 3
+connection.on("SendMessageToCallerJsMethod", function (message) {
+    //console.log("On User :", user);
+    //console.log("On Message :", message);
+
+    var templateRender = templateList(message);
+
+    console.log(templateRender);
+
+    $("#messageList").append(templateRender);
+});
+
+// start the SingalR Connection / Step 5
+connection
+    .start()
+    .then(function () {
+        //console.log("Connection Start");
+        $("#btnSendMessage").prop("disabled", false);
+    })
+    .catch(function (err) {
+        console.log(err.toString());
+        console.log("Error")
+    });
+
+// Invoke Message // Step 6
+
+$("#btnSendMessage").click(function (event) {
+    var message = $("#txtMessage").val();
+
+    //console.log("Invoke User :", userName);
+    //console.log("Invoke Message :", message);
+
+    connection
+        .invoke("SendMessage", message)
+        .catch(function (err) {
+            console.log(err.toString());
+        });
+
+    event.preventDefault();
+});
